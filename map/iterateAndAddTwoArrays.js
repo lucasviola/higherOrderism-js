e08c0e0f1725f9971da7ffabee756973ca545615
lucasviola <lucasmatzenbacher@gmail.com>
2015-09-02 15:18:13 -0300

    added first example

---
+++ map/iterateAndAddTwoArrays.js
@@ -0,0 +1,9 @@
+var firstArray = [1,2,3,4];
+var secondArray = [4,3,2,1];
+
+var addArrays = firstArray.map(function(secondArray){
+		return firstArray + secondArray;
+});
+
+console.log(addArrays);
+
