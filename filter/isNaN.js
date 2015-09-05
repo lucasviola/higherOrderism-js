ba03db485e4aaa43ff889e1626cf05517724d268
lucasviola <lucasmatzenbacher@gmail.com>
2015-09-05 13:26:42 -0300

    filter example added

---
+++ filter/isNaN.js
@@ -0,0 +1,8 @@
+var arrayOfThings = ["Array", 1, "Of", 2, "Things", 0];
+
+var isNotANumber = arrayOfThings.filter(function(x) {
+	return (typeof x != 'number');
+});
+
+console.log(isNotANumber);
+
