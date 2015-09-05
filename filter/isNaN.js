09263b849266aca0c8e40367e433b81224776d98
lucasviola <lucasmatzenbacher@gmail.com>
2015-09-05 13:27:57 -0300

    refactored using partial application

--- filter/isNaN.js
+++ filter/isNaN.js
@@ -1,7 +1,7 @@
 var arrayOfThings = ["Array", 1, "Of", 2, "Things", 0];
 
 var isNotANumber = arrayOfThings.filter(function(x) {
-	return (typeof x != 'number');
+	return ((typeof x) != 'number');
 });
 
 console.log(isNotANumber);
